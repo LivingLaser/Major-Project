e1993aee36566502cb842d8a1045ee0263507d77
LivingLaser <dassayantan450@gmail.com>
2023-04-05 13:32:10 +0530

    Login page

--- src/main/java/com/main/clint/Login.java
+++ src/main/java/com/main/clint/Login.java
@@ -1,11 +1,13 @@
 package com.main.clint;
 
 import java.io.IOException;
+import java.sql.*;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Login
@@ -14,17 +16,43 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/ecom", "root", "DBMS");
+			
+			String sql = "select name from user where email='"+email+"' and password='"+password+"'";
+			PreparedStatement pstm = con.prepareStatement(sql);
+			ResultSet rs = pstm.executeQuery();
+			
+			if(rs.next()) {
+				String color = "success";
+				String msg = "Welcome " + rs.getString("name");
+				HttpSession session = request.getSession();
+				session.setAttribute("message", msg);
+				session.setAttribute("color",color);
+				response.sendRedirect("index.jsp");
+			}
+			else {
+				String color = "danger";
+				String msg = "Wrong Email or Password";
+				HttpSession session = request.getSession();
+				session.setAttribute("message", msg);
+				session.setAttribute("color",color);
+				response.sendRedirect("login.jsp");
+			}
+		}
+		catch(Exception e) {
+			System.out.println("Exception: " + e);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -20,17 +20,17 @@
 <div class="mb-3 row">
     <label for="staticEmail" class="col-sm-2 col-form-label">Email:</label>
     <div class="col-sm-10">
-        <input type="email" class="form-control" id="inputEmail4"  required placeholder="Enter your registered email" name="" >
+        <input type="email" class="form-control" id="inputEmail4"  required placeholder="Enter your registered email" name="email" >
     </div>
   </div>
   <div class="mb-3 row">
     <label for="inputPassword" class="col-sm-2 col-form-label">Password:</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword"  required placeholder="Enter your password" name="">
+      <input type="password" class="form-control" id="inputPassword"  required placeholder="Enter your password" name="password">
     </div>
   </div>
   <div class="d-grid gap-2 col-6 mx-auto"><br>
-    <button class="btn btn-primary" type="submit" name="" >Login</button>
+    <button class="btn btn-primary" type="submit">Login</button>
     <br><br>
     <span class="text-center">Don't have an account with us? <br>Please click here to <a href="signup.jsp">Signup</a> 
     </span>
